3cc15ede0b17dd9a125fd8e7d6c09f1776762b0d
Trojnar <aliasaarn@gmail.com>
2022-10-24 14:23:57 +0200

    basic tree

---
+++ tests.py
@@ -0,0 +1,280 @@
+from unittest import TestCase
+import unittest
+from tree import TreeMap
+from tree import TreeNode
+
+
+class TestTreeMap(TestCase):
+    def setUp(self):
+        test_cases = [
+            # 0
+            (
+                ((None, 2, None), 7, (None, 6, None)),
+                1,
+                (None, 9, ((None, 5, None), 9, None)),
+            ),
+            # 1
+            (None, 1, (None, 2, (None, 3, (None, 4, None)))),
+            # 2
+            ((((None, 4, None), 3, None), 2, None), 1, None),
+            # 3
+            (
+                (
+                    ((None, 4, None), 10, (None, 12, None)),
+                    15,
+                    ((None, 18, None), 22, (None, 24, None)),
+                ),
+                25,
+                (
+                    ((None, 31, None), 25, (None, 44, None)),
+                    50,
+                    ((None, 66, None), 70, (None, 90, None)),
+                ),
+            ),
+            # 4
+            (
+                ((None, 4, None), 2, (None, 5, None)),
+                1,
+                ((None, 6, None), 3, (None, 7, None)),
+            ),
+            # 5
+            (None, 1, None),
+        ]
+        self._set_treemaps(test_cases)
+
+    def _set_treemaps(self, test_cases):
+        self.tree_maps = {
+            "tree_map": TreeMap.parse_tuple(test_cases[0]),
+            "only_right_tree_map": TreeMap.parse_tuple(test_cases[1]),
+            "only_left_tree_map": TreeMap.parse_tuple(test_cases[2]),
+            "balanced_binary_tree_4_levels_height": TreeMap.parse_tuple(test_cases[3]),
+            "balanced_binary_tree_3_levels_height": TreeMap.parse_tuple(test_cases[4]),
+            "one_node_tree": TreeMap.parse_tuple(test_cases[5])
+        }
+
+    # def _display_test_cases(self):
+    #     # Helper method for debugging
+    #     for tree_map in self.tree_maps:
+    #         tree_map.display_keys()
+
+    def test_create_tree_map_from_tuple(self):
+        self.assertEqual(self.tree_maps["tree_map"].root.key, 1)
+        self.assertEqual(self.tree_maps["tree_map"].root.parent, None)
+        self.assertEqual(self.tree_maps["tree_map"].root.left.key, 7)
+        self.assertEqual(
+            self.tree_maps["tree_map"].root.left.parent, self.tree_maps["tree_map"].root
+        )
+        self.assertEqual(self.tree_maps["tree_map"].root.right.key, 9)
+        self.assertEqual(
+            self.tree_maps["tree_map"].root.right.parent,
+            self.tree_maps["tree_map"].root,
+        )
+        self.assertEqual(self.tree_maps["tree_map"].root.left.left.key, 2)
+        self.assertEqual(
+            self.tree_maps["tree_map"].root.left.left.parent,
+            self.tree_maps["tree_map"].root.left,
+        )
+        self.assertEqual(self.tree_maps["tree_map"].root.left.right.key, 6)
+        self.assertEqual(
+            self.tree_maps["tree_map"].root.left.right.parent,
+            self.tree_maps["tree_map"].root.left,
+        )
+        self.assertEqual(self.tree_maps["tree_map"].root.right.right.key, 9)
+        self.assertEqual(
+            self.tree_maps["tree_map"].root.right.right.parent,
+            self.tree_maps["tree_map"].root.right,
+        )
+        self.assertEqual(self.tree_maps["tree_map"].root.right.left.key, None)
+        self.assertEqual(
+            self.tree_maps["tree_map"].root.right.left.parent,
+            self.tree_maps["tree_map"].root.right,
+        )
+
+        self.assertEqual(self.tree_maps["only_right_tree_map"].root.key, 1)
+        self.assertEqual(self.tree_maps["only_right_tree_map"].root.left.key, None)
+        self.assertEqual(self.tree_maps["only_right_tree_map"].root.right.key, 2)
+        self.assertEqual(self.tree_maps["only_right_tree_map"].root.right.right.key, 3)
+        self.assertEqual(
+            self.tree_maps["only_right_tree_map"].root.right.right.right.key, 4
+        )
+        self.assertEqual(
+            self.tree_maps["only_right_tree_map"].root.right.right.right.right.key, None
+        )
+        # parents
+        self.assertEqual(self.tree_maps["tree_map"].root.parent, None)
+        self.assertEqual(
+            self.tree_maps["only_right_tree_map"].root.right.parent,
+            self.tree_maps["only_right_tree_map"].root,
+        )
+        self.assertEqual(
+            self.tree_maps["only_right_tree_map"].root.right.right.parent,
+            self.tree_maps["only_right_tree_map"].root.right,
+        )
+        self.assertEqual(
+            self.tree_maps["only_right_tree_map"].root.right.right.right.parent,
+            self.tree_maps["only_right_tree_map"].root.right.right,
+        )
+        self.assertEqual(
+            self.tree_maps["only_right_tree_map"].root.right.right.right.right.parent,
+            self.tree_maps["only_right_tree_map"].root.right.right.right,
+        )
+
+        self.assertEqual(self.tree_maps["only_left_tree_map"].root.key, 1)
+        self.assertEqual(self.tree_maps["only_left_tree_map"].root.right.key, None)
+        self.assertEqual(self.tree_maps["only_left_tree_map"].root.left.key, 2)
+        self.assertEqual(self.tree_maps["only_left_tree_map"].root.left.left.key, 3)
+        self.assertEqual(
+            self.tree_maps["only_left_tree_map"].root.left.left.left.key, 4
+        )
+        self.assertEqual(
+            self.tree_maps["only_left_tree_map"].root.left.left.left.left.key, None
+        )
+        # parents
+        self.assertEqual(self.tree_maps["tree_map"].root.parent, None)
+        self.assertEqual(
+            self.tree_maps["only_left_tree_map"].root.right.parent,
+            self.tree_maps["only_left_tree_map"].root,
+        )
+        self.assertEqual(
+            self.tree_maps["only_left_tree_map"].root.left.parent,
+            self.tree_maps["only_left_tree_map"].root,
+        )
+        self.assertEqual(
+            self.tree_maps["only_left_tree_map"].root.left.left.parent,
+            self.tree_maps["only_left_tree_map"].root.left,
+        )
+        self.assertEqual(
+            self.tree_maps["only_left_tree_map"].root.left.left.left.parent,
+            self.tree_maps["only_left_tree_map"].root.left.left,
+        )
+        self.assertEqual(
+            self.tree_maps["only_left_tree_map"].root.left.left.left.left.parent,
+            self.tree_maps["only_left_tree_map"].root.left.left.left,
+        )
+
+    def test_to_tuple_method(self):
+
+        self.assertEqual(
+            self.tree_maps["tree_map"].to_tuple(),
+            (
+                ((None, 2, None), 7, (None, 6, None)),
+                1,
+                (None, 9, ((None, 5, None), 9, None)),
+            ),
+        )
+        self.assertEqual(
+            self.tree_maps["only_right_tree_map"].to_tuple(),
+            (None, 1, (None, 2, (None, 3, (None, 4, None)))),
+        )
+        self.assertEqual(
+            self.tree_maps["only_left_tree_map"].to_tuple(),
+            ((((None, 4, None), 3, None), 2, None), 1, None),
+        )
+
+    def test_height_method(self):
+        self.assertEqual(self.tree_maps["tree_map"].height(), 4)
+        self.assertEqual(self.tree_maps["only_right_tree_map"].height(), 4)
+        self.assertEqual(self.tree_maps["only_left_tree_map"].height(), 4)
+
+    def test_length_method(self):
+        self.assertEqual(self.tree_maps["tree_map"].length(), 7)
+        self.assertEqual(self.tree_maps["only_right_tree_map"].length(), 4)
+        self.assertEqual(self.tree_maps["only_left_tree_map"].length(), 4)
+
+    def test_min_depth_method(self):
+        self.assertEqual(self.tree_maps["tree_map"].min_depth(), 2)
+        self.assertEqual(self.tree_maps["only_right_tree_map"].min_depth(), 1)
+        self.assertEqual(self.tree_maps["only_left_tree_map"].min_depth(), 1)
+
+    def test_traverse_inorder(self):
+        self.assertEqual(
+            self.tree_maps["balanced_binary_tree_4_levels_height"].traverse_inorder(),
+            [4, 10, 12, 15, 18, 22, 24, 25, 31, 25, 44, 50, 66, 70, 90],
+        )
+        self.assertEqual(
+            self.tree_maps["balanced_binary_tree_3_levels_height"].traverse_inorder(),
+            [4, 2, 5, 1, 6, 3, 7],
+        )
+
+    def test_traverse_preorder(self):
+        self.assertEqual(
+            self.tree_maps["balanced_binary_tree_4_levels_height"].traverse_preorder(),
+            [25, 15, 10, 4, 12, 22, 18, 24, 50, 25, 31, 44, 70, 66, 90],
+        )
+        self.assertEqual(
+            self.tree_maps["balanced_binary_tree_3_levels_height"].traverse_preorder(),
+            [1, 2, 4, 5, 3, 6, 7],
+        )
+
+    def test_traverse_postorder(self):
+        self.assertEqual(
+            self.tree_maps["balanced_binary_tree_4_levels_height"].traverse_postorder(),
+            [4, 12, 10, 18, 24, 22, 15, 31, 44, 25, 66, 90, 70, 50, 25],
+        )
+        self.assertEqual(
+            self.tree_maps["balanced_binary_tree_3_levels_height"].traverse_postorder(),
+            [4, 5, 2, 6, 7, 3, 1],
+        )
+
+    def test_insert_method(self):
+        # blank on the left at 3rd level in 5 level tree
+        self.tree_maps["tree_map"].display_keys()
+        self.tree_maps["tree_map"].insert(1, 2)
+        self.tree_maps["tree_map"].display_keys()
+        self.tree_maps["tree_map"].insert(3, 2)
+        self.tree_maps["tree_map"].display_keys()
+        self.tree_maps["tree_map"].insert(8, 2)
+        self.tree_maps["tree_map"].display_keys()
+
+        print("-------------------------")
+        # Blank on the right at 3rd level in 5 level tree
+        self.tree_map = TreeMap.parse_tuple(            (
+                ((None, 2, None), 7, None),
+                1,
+                ((None, 5, None), 9, ((None, 5, None), 9, None)),
+            ))
+        self.tree_map.display_keys()
+        self.tree_map.insert(2, 2)
+        self.tree_map.display_keys()
+        print("----------------------------")
+
+        # Blank last one at 5th level
+        self.tree_map_last_blank = TreeMap.parse_tuple(            (
+                ((((None, 6, None), 2, (None, 10, None)), 7, ((None, 11, None), 5, (None, 12, None))), 1, (((None, 19, None), 5, (None, 20, None)), 9, ((None, 5, None), 9, None)))
+            ))
+        self.tree_map_last_blank.display_keys()
+        self.tree_map_last_blank.insert(3, 8)
+        self.tree_map_last_blank.display_keys()
+
+
+        # one node tree
+        self.tree_maps["one_node_tree"].insert(2, None)
+        self.tree_maps["one_node_tree"].insert(3, None)
+        self.assertEqual(self.tree_maps["one_node_tree"].root.left.key, 2)
+        self.assertEqual(self.tree_maps["one_node_tree"].root.left.parent.key, 1)
+        self.assertEqual(self.tree_maps["one_node_tree"].root.right.key, 3)
+        self.assertEqual(self.tree_maps["one_node_tree"].root.right.parent.key, 1)
+
+
+        # Only right tree
+        self.tree_maps["only_left_tree_map"].insert(5, None)
+        self.tree_maps["only_left_tree_map"].insert(6, 8)
+        self.assertEqual(self.tree_maps["only_left_tree_map"].root.right.key, 5)
+        self.assertEqual(self.tree_maps["only_left_tree_map"].root.right.parent.key, 1)
+        self.assertEqual(self.tree_maps["only_left_tree_map"].root.left.right.key, 6)
+        self.assertEqual(self.tree_maps["only_left_tree_map"].root.left.right.value, 8)
+        self.assertEqual(self.tree_maps["only_left_tree_map"].root.left.right.parent.key, 2)
+
+        # Only left tree
+        self.tree_maps["only_right_tree_map"].insert(7, None)
+        self.tree_maps["only_right_tree_map"].insert(8, None)
+        self.assertEqual(self.tree_maps["only_right_tree_map"].root.left.key, 7)
+        self.assertEqual(self.tree_maps["only_right_tree_map"].root.left.parent.key, 1)
+        self.assertEqual(self.tree_maps["only_right_tree_map"].root.left.left.key, 8)
+        self.assertEqual(self.tree_maps["only_right_tree_map"].root.left.left.parent.key, 7)
+
+        # Values inserted
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tree.py
@@ -0,0 +1,401 @@
+from __future__ import annotations
+from typing import Optional
+
+
+class TreeNode:
+    """
+    Class that represents node of Tree.
+
+    Attributes
+    ----------
+    key
+        Key of the node
+    value
+        Value assigned to given key
+    left : TreeNode
+        Left child node (default None)
+    right : TreeNode
+        Right child node (default None)
+    parent : TreeNode
+        Parent node.
+
+    """
+
+    def __init__(self, key, value, left=None, right=None, parent=None):
+        self.__key = key
+        self.value = value
+        self.__left = left
+        self.__right = right
+        self.__parent = parent
+
+    @property
+    def key(self):
+        return self.__key
+
+    @property
+    def left(self):
+        return self.__left
+
+    @property
+    def right(self):
+        return self.__right
+
+    @property
+    def parent(self):
+        return self.__parent
+
+    @left.setter
+    def left(self, new_left):
+        if isinstance(new_left, TreeNode):
+            self.__left = new_left
+        else:
+            raise Exception("Data type of the attribute left should be TreeNode().")
+
+    @right.setter
+    def right(self, new_right):
+        if isinstance(new_right, TreeNode):
+            self.__right = new_right
+        else:
+            raise Exception("Data type of the attribute right should be TreeNode().")
+
+    @parent.setter
+    def parent(self, new_parent):
+        if isinstance(new_parent, TreeNode):
+            self.__parent = new_parent
+        else:
+            raise Exception("Data type of the attribute parent should be TreeNode().")
+
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__}({self.key}, {self.value}, {self.left}, "
+            f"{self.right}, {self.parent})"
+        )
+
+    def __str__(self):
+        return str(self.key)
+
+    @staticmethod
+    def __is_none(object):
+        # Leaf's left/right element can be None or TreeNode(None, None)
+        # this helper method identify if self is blank element or not.
+        if object is None or (isinstance(object, TreeNode) and object.key == None):
+            return True
+        return False
+
+    @staticmethod
+    def __is_leaf(object):
+        # Return True if node is leaf.
+        if TreeNode.__is_none(object.left) and TreeNode.__is_none(object.right):
+            return True
+        return False
+
+    def to_tuple(self) -> Optional[tuple]:
+        if TreeNode.__is_none(self):
+            return None
+
+        return (
+            TreeNode.to_tuple(self.left),  # type: ignore
+            self.key,
+            TreeNode.to_tuple(self.right),  # type: ignore
+        )
+
+    def height(self):
+        if TreeNode.__is_none(self):
+            return 0
+
+        return 1 + max(
+            TreeNode.height(self.left), TreeNode.height(self.right)  # type: ignore
+        )
+
+    def display_keys(self, level=0):
+        if TreeNode.__is_none(self):
+            print(level * "\t", "Ø")
+            return
+
+        if TreeNode.__is_leaf(self):
+            print(level * "\t", self.key)
+            return
+
+        TreeNode.display_keys(self.right, level + 1)  # type: ignore
+        print(level * "\t", self.key)
+        TreeNode.display_keys(self.left, level + 1)  # type: ignore
+
+    def length(self):
+        if TreeNode.__is_none(self):
+            return 0
+
+        return (
+            1 + TreeNode.length(self.left) + TreeNode.length(self.right)  # type: ignore
+        )
+
+    def min_depth(self):
+        if TreeNode.__is_none(self):
+            return 0
+
+        return 1 + min(
+            TreeNode.min_depth(self.left), TreeNode.min_depth(self.right)  # type: ignore
+        )
+
+    def traverse_inorder(self) -> list:
+        # left root right
+        if TreeNode.__is_none(self):
+            return []
+
+        return (
+            TreeNode.traverse_inorder(self.left)  # type: ignore
+            + [self.key]
+            + TreeNode.traverse_inorder(self.right)  # type: ignore
+        )
+
+    def traverse_preorder(self) -> list:
+        # root left rigth
+        if TreeNode.__is_none(self):
+            return []
+        return (
+            [self.key]
+            + TreeNode.traverse_preorder(self.left)  # type: ignore
+            + TreeNode.traverse_preorder(self.right)  # type: ignore
+        )
+
+    def traverse_postorder(self) -> list:
+        # right left root
+        if TreeNode.__is_none(self):
+            return []
+
+        return (
+            TreeNode.traverse_postorder(self.left)  # type: ignore
+            + TreeNode.traverse_postorder(self.right)  # type: ignore
+            + [self.key]
+        )
+
+    def insert(self, node):
+        self.__insert(node, self.min_depth())
+
+    def __insert(self, node, min_depth, depth=1, inserted=False):
+        print(self, inserted, min_depth, depth)
+        if TreeNode.__is_none(self.left) and not inserted and depth == min_depth:
+            print(1)
+            self.left = node
+            self.left.parent = self
+            return True
+        elif TreeNode.__is_none(self.right) and not inserted and depth == min_depth:
+            print(2)
+            self.right = node
+            self.right.parent = self
+            return True
+        elif TreeNode.__is_none(self.left) and TreeNode.__is_none(self.right):
+            # If node is a leaf
+            return False
+
+        if not inserted:
+            inserted = TreeNode.__insert(self.left, node, min_depth, depth + 1, inserted)  # type: ignore
+            if not inserted:
+                inserted = TreeNode.__insert(self.right, node, min_depth, depth + 1, inserted)  # type: ignore
+        print(3)
+        if inserted:
+            return True
+        else:
+            return False
+
+
+class TreeMap:
+    """
+    Class that provides mapping of tree nodes. Mapping begins at node provided as root
+    attribute up.
+
+    Attributes
+    ----------
+    root: TreeNode
+        Initial node of the tree.
+
+    Methods
+    ---------
+    insert(node : TreeNode)
+        Inserts node to TreeMap instance's tree.
+    find(key) -> TreeNode
+        Returns node of given key or None if not found.
+    update(key, value) -> boolean
+        Returns True if updated or False if not found.
+    display_keys()
+        Displays tree in standard output.
+    to_list()
+        Returns tree converted to list format [node1, node2, ..., nodeN].
+    to_tuple()
+        Returns tree converted to tuple format (left_subtree, node, right_subtree).
+    traverse_preorder() -> list
+        Traverse tree preorder. Returns list.
+    traverse_inorder() -> list
+        Traverse tree inorder. Returns list.
+    traverse_postorder() -> list
+        Traverse tree postorder. Returns list.
+    is_complete() -> boolean
+        Checks if tree is complete.
+    height() -> int
+        Returns tree height.
+    len() -> int
+        Returns tree length.
+
+
+    """
+
+    def __init__(self, root: TreeNode):
+        self.root = root
+
+    def insert(self, key, value):
+        """
+        Allows to insert node to the tree. Node is added to the first available place
+        from the left.
+
+        Parameters
+        ----------
+        node
+            Node to insert.
+        """
+        node = TreeNode(key, value)
+        print(self.root.insert(node))
+
+    def find(self, key) -> TreeNode:
+        """
+        Finds node of given key.
+
+        Parameters
+        ----------
+        key
+            Key of wanted node.
+
+        Returns
+        -------
+        TreeNode
+            Node of given key or None if not found.
+        """
+        pass
+
+    def update(self, key, value) -> bool:
+        """
+        Updates node.
+
+        Parameters
+        ----------
+        key
+            Key of the node to update
+        value
+            Value to update.
+        """
+        pass
+
+    def display_keys(self, root=None, level=0):
+        """Displays keys in tree-like form at stdout."""
+        self.root.display_keys()
+
+    def to_tuple(self) -> Optional[tuple]:
+        """
+        Returns
+        -------
+        tuple
+            Tuple organized in form of (left_subtree, key, right_subtree)
+        """
+        return self.root.to_tuple()
+
+    @classmethod
+    def parse_tuple(cls, tree_t: tuple) -> TreeMap:
+        """
+        Parse given tuple organized in form (left_subtree, key, right_subtree).
+
+        Parameters
+        ----------
+        tree : tuple
+            tuple of key-value paris of the tree organized in form of
+            (left_subtree, key-value, right_subtree).
+
+        Returns
+        --------
+        TreeMap
+            Map of the tree created from tuple.
+        """
+
+        return cls(cls.__create_tree_from_tuple(tree_t))
+
+    @staticmethod
+    def __create_tree_from_tuple(tree_t: tuple):
+        """Creates tree from tuple and returns root node."""
+        if tree_t is None:
+            return TreeNode(None, None)
+        tree_node = TreeNode(tree_t[1], tree_t[1])
+        tree_node.left = TreeMap.__create_tree_from_tuple(tree_t[0])
+        tree_node.left.parent = tree_node
+        tree_node.right = TreeMap.__create_tree_from_tuple(tree_t[2])
+        tree_node.right.parent = tree_node
+        return tree_node
+
+    def traverse_preorder(self) -> list:
+        """
+        Traverse tree preorder. Returns list.
+
+        Returns
+        --------
+        list
+            List of nodes in preorder.
+
+        """
+        return self.root.traverse_preorder()
+
+    def traverse_postorder(self) -> list:
+        """
+        Traverse tree post. Returns list.
+
+        Returns
+        --------
+        list
+            List of nodes postorder.
+        """
+        return self.root.traverse_postorder()
+
+    def traverse_inorder(self) -> list:
+        """
+        Traverse tree post. Returns list.
+
+        Returns
+        --------
+        list
+            List of nodes inorder.
+
+        """
+        return self.root.traverse_inorder()
+
+    def is_complete(self) -> bool:
+        """
+        Checks if tree is complete
+
+        Returns
+        --------
+        boolean
+            True if tree is complete, False if not
+        """
+        return True
+
+    def height(self) -> int:
+        """
+        Returns
+        -------
+        int
+            Height/depth of the tree.
+        """
+        return self.root.height()
+
+    def length(self) -> int:
+        """
+        Returns
+        -------
+        int
+            Amount of the elements in the tree.
+        """
+        return self.root.length()
+
+    def min_depth(self) -> int:
+        """
+        Returns
+        -------
+        int
+            Minimal height/depth of the tree.
+        """
+        return self.root.min_depth()
